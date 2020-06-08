6b99830d44a7e65074f6b69a5d287abc96274d8b
Tai Truong <giaptai170@gmail.com>
2020-06-08 16:04:58 +0700

    bhhh

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: /<controller>/
+        private readonly ILogger<HomeController> _logger;
+        private readonly BookService _bookService;
+
+        public BookController(ILogger<HomeController> logger, BookService bookService)
+        {
+            _logger = logger;
+            _bookService = bookService;
+        }
+        [HttpGet("{id}")]
+        public IActionResult Detail(int id)
+        {
+            var books = _bookService.ListAllBooks();
+
+            return View(books);
+        }
+    }
+}
---
+++ BookStore/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly BookService _bookService;
+
+        public CategoryController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Detail(int id)
+        {
+            ViewData["GetAllBookOfCategory"] = _bookService.GetAllBookOfCategory(id);
+            ViewData["list1"] = _bookService.GetCountCategory();
+            var books = _bookService.ListAllBooks();
+
+            return View(books);
+        }
+    }
+}
